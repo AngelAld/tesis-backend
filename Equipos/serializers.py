c3189e8774804b56a3ee29a4541ba4fc6824c6c8
AngelAld <aaaldana50@gmail.com>
2024-11-08 17:12:31 -0500

    Corrección de serializer de equipos

--- Equipos/serializers.py
+++ Equipos/serializers.py
@@ -170,50 +170,50 @@ class EquipoSerializer(ModelSerializer):
             "area_id": {"write_only": True},
         }
 
-        @atomic
-        def create(self, validated_data):
-            hostname = validated_data.get("hostname")
-            ip = validated_data.get("ip")
-            estado = True
-            last_update = now()
-            estado_equipo = estadoEquipoDefault
-            estado_alerta = estadoAlertaDefault
-            area = getAreaByIp(ip)
-            # hardware
+    @atomic
+    def create(self, validated_data):
+        hostname = validated_data.get("hostname")
+        ip = validated_data.get("ip")
+        estado = True
+        last_update = now()
+        estado_equipo = estadoEquipoDefault
+        estado_alerta = estadoAlertaDefault
+        area = getAreaByIp(ip)
+        # hardware
 
-            cpus = validated_data.pop("cpu_set", [])
-            memorys = validated_data.pop("memory_set", [])
-            disks = validated_data.pop("disk_set", [])
+        cpus = validated_data.pop("cpu_set", [])
+        memorys = validated_data.pop("memory_set", [])
+        disks = validated_data.pop("disk_set", [])
 
-            equipo, created = Equipo.objects.get_or_create(
-                hostname=hostname,
-                defaults={
-                    "ip": ip,
-                    "estado": estado,
-                    "area": area,
-                    "last_update": last_update,
-                    "estado_equipo": estado_equipo,
-                    "estado_alerta": estado_alerta,
-                },
-            )
-            if not created:
-                equipo.last_update = last_update
-                equipo.ip = ip
-                equipo.area = area
-                equipo.save()
+        equipo, created = Equipo.objects.get_or_create(
+            hostname=hostname,
+            defaults={
+                "ip": ip,
+                "estado": estado,
+                "area": area,
+                "last_update": last_update,
+                "estado_equipo": estado_equipo,
+                "estado_alerta": estado_alerta,
+            },
+        )
+        if not created:
+            equipo.last_update = last_update
+            equipo.ip = ip
+            equipo.area = area
+            equipo.save()
 
-            for cpu in cpus:
-                cpu["equipo"] = equipo
-                CpuSerializer().create(cpu)
+        for cpu in cpus:
+            cpu["equipo"] = equipo
+            CpuSerializer().create(cpu)
 
-            for memory in memorys:
-                memory["equipo"] = equipo
-                MemorySerializer().create(memory)
-            for disk in disks:
-                disk["equipo"] = equipo
-                DiskSerializer().create(disk)
+        for memory in memorys:
+            memory["equipo"] = equipo
+            MemorySerializer().create(memory)
+        for disk in disks:
+            disk["equipo"] = equipo
+            DiskSerializer().create(disk)
 
-            return equipo
+        return equipo
 
 
 class SensoresSerializer(serializers.Serializer):
